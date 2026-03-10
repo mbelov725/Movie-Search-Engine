cda305ab262e53bcec63ed291b2e3e962a12566b
mbelov725 <mark.belov725@gmail.com>
2026-03-10 15:20:47 -0400

    Removed server backend, for deployment.

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,17 @@
+const BASE_URL = "https://api.themoviedb.org/3";
+
 export const getPopularMovies = async () => {
-  const response = await fetch("/api/movies/popular");
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
-  return data;
+  return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `/api/movies/search?q=${encodeURIComponent(query)}`,
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`,
   );
   const data = await response.json();
-  return data;
+  return data.results;
 };
